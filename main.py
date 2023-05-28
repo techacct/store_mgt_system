129f19dcf53a4fc56867de9c5fe81d53287e68b3
Ademola Oladapo <ademolaoladapo@gmail.com>
2023-05-28 09:43:14 +0100

    using csv to load instances (#1)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     discounted_price = 0.8 
     all = []
@@ -22,13 +24,22 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.discounted_price
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as csv_file:
+           reader = csv.DictReader(csv_file)
+           items = list(reader)
+
+        for item in items:
+            
+            Item(
+            name=item.get('name'),
+            price=float(item.get('price')),
+            quantity=int(item.get('quantity'))
+        )
+
     def __repr__(self):
         return f"Item({self.name}, {self.price}, {self.quantity})"
 
-item1 = Item("Phone", 100, 5)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouser", 50, 5)
-item5 = Item("Keyboard", 75, 5)
-
+Item.instantiate_from_csv()
 print(Item.all)
