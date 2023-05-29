c2d6ccc7ffc9df2e807764a6c3a73c5cb7afb9c3
techacct <ademolaoladapo@gmail.com>
2023-05-29 09:35:11 +0100

    cleanup

--- phone.py
+++ phone.py
@@ -1,7 +1,7 @@
 from item import Item
 
 class Phone(Item):
-
+    
     def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
         #call the super function to have access to all attributes/methods
         super().__init__(name, price, quantity)
@@ -11,4 +11,3 @@ class Phone(Item):
 
         #assignment to self  objects
         self.broken_phones = broken_phones
-        
